9c0d3897bd52cf0462ddd0731280e5265cb6af54
Arden de Raaij <a.de.raaij@gmail.com>
2018-01-01 15:52:10 +0000

    [update] with react class components and binding

--- app.js
+++ app.js
@@ -1,65 +1,102 @@
-const root = document.getElementById('root');
-const divStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    alignItems: 'center',
-};
+// const root = document.getElementById('root');
+// const divStyle = {
+//     display: 'flex',
+//     justifyContent: 'center',
+//     flexDirection: 'column',
+//     alignItems: 'center',
+// };
 
-const buttonStyle = {
-    margin: '1em 0',
-    padding: '1em 2em',
-    display: 'block',
-    background: 'transparent',
-    border: '1px solid #999999',
-    fontWeight: '700',
-    width: 120,
-};
+// const buttonStyle = {
+//     margin: '1em 0',
+//     padding: '1em 2em',
+//     display: 'block',
+//     background: 'transparent',
+//     border: '1px solid #999999',
+//     fontWeight: '700',
+//     width: 120,
+// };
 
-class StopWatch extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            lapse: 0,
-            running: false,
-        }
+// class StopWatch extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { 
+//             lapse: 0,
+//             running: false,
+//         }
+//     }
+
+//     handleRunClick = () => {
+//         this.setState(state => {
+//             if (state.running) {
+//                clearInterval(this.timer);
+//             } else {
+//                 this.timer = setInterval(() => {
+//                     this.setState({ lapse: Date.now() - startTime });  
+//                 });
+//             }
+//             return { running: !state.running }
+//         })
+//         const startTime = Date.now() - this.state.lapse;
+//     }
+
+//     handleClearClick = () => {
+//         clearInterval(this.timer);
+//         this.setState({ lapse: 0, running: false, });
+//     }
+
+//     componentWillUnmount() {
+//         clearInterval(this.timer);
+//     }
+
+//     render() {
+//         const { lapse, running } = this.state;
+//         return (
+//             <div style={divStyle}>
+//                 <label style={{ fontSize: '2em', fontWeight: '700' }}>
+//                     {lapse}ms
+//                 </label>
+//                 <button onClick={this.handleRunClick} style={buttonStyle}>{running ? 'Stop' : 'Start'}</button>
+//                 <button onClick={this.handleClearClick} style={buttonStyle}>Clear</button>
+//             </div>
+//         )
+//     }
+// }
+
+// ReactDOM.render(<StopWatch />,root);
+
+const rootEl = document.querySelector('#root');
+
+
+
+// class ClickButton extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <button>1</button>
+//             </div>
+//         )
+//     }
+// }
+
+class ClickButton extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            counter: 0,
+        };
     }
 
-    handleRunClick = () => {
-        this.setState(state => {
-            if (state.running) {
-               clearInterval(this.timer);
-            } else {
-                this.timer = setInterval(() => {
-                    this.setState({ lapse: Date.now() - startTime });  
-                });
-            }
-            return { running: !state.running }
-        })
-        const startTime = Date.now() - this.state.lapse;
-    }
-
-    handleClearClick = () => {
-        clearInterval(this.timer);
-        this.setState({ lapse: 0, running: false, });
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timer);
+    clickHandler() {
+        this.setState(({ counter }) => ({
+            counter: counter + 1,
+        }));
     }
 
     render() {
-        const { lapse, running } = this.state;
-        return (
-            <div style={divStyle}>
-                <label style={{ fontSize: '2em', fontWeight: '700' }}>
-                    {lapse}ms
-                </label>
-                <button onClick={this.handleRunClick} style={buttonStyle}>{running ? 'Stop' : 'Start'}</button>
-                <button onClick={this.handleClearClick} style={buttonStyle}>Clear</button>
-            </div>
+        return(
+            <button onClick={this.clickHandler}>{this.state.counter}</button>
         )
     }
 }
 
-ReactDOM.render(<StopWatch />,root);
+ReactDOM.render(<ClickButton />, rootEl);
--- readme.md
+++ readme.md
@@ -84,3 +84,75 @@ Creating a stateless component: Create a function that returns a JSX element, an
 - Extending the React class Component is nothing more than using an ES6 Class created by the React team. `React.createClass` was their own proprietary solution which they've since backed away from.
 - A stateless / functional component is exactly what the name implies. Don't worry too much about which one you're using, switching them out is not that hard and the performance gain of using stateless components isn't always that high. Linters be damned.
 - When you're thinking about sending data in the form of `props` or `state` from a child component to a parent component, ask yourself if it's really necessary. Most of the time you already have data available in the parent component (*needs example*)
+
+
+## Lessons
+
+### 12. [Using Class components with React](https://egghead.io/lessons/egghead-use-class-components-with-react)
+
+The name of this lesson might be slightly misleading as it's more about event handling and binding within the React class component. I might move this part under 'event-handling'.
+
+Working with React class components (and ES6 classes in general) means making a lot of use of `this` to reference or declare internal functions and variables. When you create a function in the *upper scope of a class component you can call that function within other functions or components by adding `this.` to the function call:
+
+```javascript
+class ClickButton extends React.Component {
+    clickHandler() {
+        alert('You\'ve clicked 🎉')
+    }
+
+    render() {
+        return(
+            <button onClick={this.clickHandler}>Click</button>
+        )
+    }
+}
+```
+
+The example above will work with no problem. We define a function in the upper scope and refer to it within ther `render` function by adding `this.` to the function call.
+
+But what if you want to update / set the state in the function you call in your eventhandler?
+
+```javascript
+class ClickButton extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            counter: 0,
+        };
+    }
+
+    clickHandler() {
+        this.setState(({ counter }) => ({
+            counter: counter + 1,
+        }));
+    }
+
+    render() {
+        return(
+            <button onClick={this.clickHandler}>{this.state.counter}</button>
+        )
+    }
+}
+```
+
+In the example above we first call the `constructor` method. This is where we set the state. Note how we call `state` with `this.` infront of it. You could also define state on the same level as the `constructor`, withouth the `this`.
+
+In our clickhandler, we now update the state with `setState` and add an extra count to the counter every time we click the button. It looks like it should work, but it doesn't though. The error message reads something like `Cannot read property 'setState' of undefined`.
+
+As it turns out, `setState` is undefined because `this` doesn't refer to the upprscope anymore. We call the function within another function which means the `this` used with `setState` points to the wrong place. To fix this we can do a couple of things. One of the easiest ways to solve this is changing the `clickHandler` function to an arrow function:
+
+```javascript
+clickHandler = () => {
+    this.setState(({ counter }) => ({
+        counter: counter + 1,
+    }));
+}
+```
+
+This is the ['stage 3 public class field syntax proposal'](https://github.com/tc39/proposal-class-fields) and seems to be the recommended way of binding your function. Read more about the different possibilities in these great articles and the React Docs:
+
+- [Why Arrow Functions and bind in React’s Render are Problematic](https://medium.freecodecamp.org/why-arrow-functions-and-bind-in-reacts-render-are-problematic-f1c08b060e36)
+- [React Binding Patterns: 5 Approaches for Handling `this`](https://medium.freecodecamp.org/react-binding-patterns-5-approaches-for-handling-this-92c651b5af56)
+- [Passing functions to components - React Docs](https://reactjs.org/docs/faq-functions.html#how-do-i-bind-a-function-to-a-component-instance)
+
+*is this the right terminology?
